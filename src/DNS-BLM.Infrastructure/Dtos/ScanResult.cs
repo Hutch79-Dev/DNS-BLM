f0429719a850796c088547b435fbd2d5ebd164e3
Hutch79 <42042811+Hutch79@users.noreply.github.com>
2025-04-23 09:52:40 +0200

    Remove this redundant primary constructor (#25)

--- src/DNS-BLM.Infrastructure/Dtos/ScanResult.cs
+++ src/DNS-BLM.Infrastructure/Dtos/ScanResult.cs
@@ -1,6 +1,6 @@
 namespace DNS_BLM.Infrastructure.Dtos;
 
-public class ScanResult()
+public class ScanResult
 {
     public required string Domain { get; set; }
     public required string ScannerName { get; set; }
