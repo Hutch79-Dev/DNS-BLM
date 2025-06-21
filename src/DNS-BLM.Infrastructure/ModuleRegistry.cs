c080348f026af5781c6f8043727e5a9c71bdf9e8
Hutch79 <me@hutch79.ch>
2025-06-21 23:25:12 +0200

    Changed VirusTotalService registration from sigleton to scoped

--- src/DNS-BLM.Infrastructure/ModuleRegistry.cs
+++ src/DNS-BLM.Infrastructure/ModuleRegistry.cs
@@ -4,9 +4,7 @@ using DNS_BLM.Infrastructure.Services;
 using DNS_BLM.Infrastructure.Services.NotificationServices;
 using DNS_BLM.Infrastructure.Services.ScannerServices;
 using DNS_BLM.Infrastructure.Services.ServiceInterfaces;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 namespace DNS_BLM.Infrastructure;
 
@@ -20,7 +18,7 @@ public static class ModuleRegistry
 
         string? virusTotalApiKey = appConfiguration.ApiCredentials.VirusTotal;
 
-        services.AddSingleton<IBlacklistScanner, VirusTotalService>();
+        services.AddScoped<IBlacklistScanner, VirusTotalService>();
         services.AddHttpClient("VirusTotal", client =>
         {
             client.BaseAddress = new Uri("https://www.virustotal.com/api/v3/");
